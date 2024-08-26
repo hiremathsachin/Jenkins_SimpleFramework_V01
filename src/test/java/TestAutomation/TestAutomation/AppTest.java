ea74f73fe6d7a40785d8d1f4da44e5319da6530e
hiremathsachin <103627771+hiremathsachin@users.noreply.github.com>
2024-08-26 05:48:36 +0530

    first jenkins job pushing to my repo

---
+++ src/test/java/TestAutomation/TestAutomation/AppTest.java
@@ -0,0 +1,23 @@
+package TestAutomation.TestAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.quit();
+        
+        
+    }
+}
